5c0e2fe283979951627aca1b037c64b2dd783378
Lee Morgan <lee@leemorgan.dev>
2025-02-24 16:51:24 -0500

    Add function to create a new user.

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,29 @@
 import User from "../models/User.js";
-import {HttpError} from "../HttpError.js";
 import validate from "../validation/user.js";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 const createUserRoute = (req, res, next)=>{
     try{
         validate(req.body);
-        const pass = hashPass(req.body.password);
-        const user = createUser(req.body, pass);
+        const user = createUser(req.body);
         await user.save();
         res.json(user);
     }catch(e){next(e)}
+}
+
+/*
+ Create a new user object
+ @param {Object} data - Data from request body
+ @return {User} User object
+ */
+const createUser = (data)=>{
+    return new User({
+        name: data.name,
+        email: data.email.toLowerCase(),
+        password: hashPass(data.password),
+        uuid: newUuid()
+    });
 }
 
 /*
@@ -20,6 +33,14 @@ const createUserRoute = (req, res, next)=>{
  */
 const hashPass = (pass)=>{
     return await bcrypt.hash(password, 10);
+}
+
+/*
+ Create new UUID
+ @return {String} New UUID
+ */
+const newUuid = ()=>{
+    return crypto.randomUUID();
 }
 
 export {
