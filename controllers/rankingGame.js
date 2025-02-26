90cd9c6579729ca8ce80329f026b624710b0074d
Lee Morgan <lee@leemorgan.dev>
2025-02-26 17:40:16 -0500

    Add route to create a new ranking game.

--- controllers/rankingGame.js
+++ controllers/rankingGame.js
@@ -8,6 +8,35 @@ const createGameRoute = async (req, res, next)=>{
     }catch(e){next(e)}
 }
 
+/*
+ Create a new game
+ @param {Object} data - Data from the request
+ @param {User} user - User object
+ @return {Game} Ranking Game object
+ */
+const createGame = (data, user)=>{
+    return new Game({
+        players: [],
+        owner: user._id,
+        season: data.season,
+        part: data.part
+    });
+}
+
+/*
+ Create modified game object for frontend
+ @param {Game} game - Game object
+ @return {Object} Modified game object
+ */
+const responseGame = (game)=>{
+    return {
+        players: game.players,
+        owner: game.owner.toString(),
+        season: game.season,
+        part: game.part
+    };
+}
+
 export {
     createGameRoute
 }
