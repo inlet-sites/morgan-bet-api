53fdabd25f461d988506a8878af3a1fee6301aab
Lee Morgan <lee@leemorgan.dev>
2025-02-26 15:08:34 -0500

    Create model for games.

--- models/game.js
+++ models/game.js
@@ -1,4 +1,56 @@
 import mongoose from "mongoose";
 
 const GameSchema  = new mongoose.Schema({
-})
+    apiId: {
+        type: Number,
+        required: true
+    },
+    homeTeam: {
+        team: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        hits: {
+            type: Number,
+            required: false
+        },
+        runs: {
+            type: Number,
+            required: false
+        },
+        errors: {
+            type: Number,
+            required: false
+        },
+        inningScores: {
+            type: [Number],
+            required: false
+        }
+    },
+    awayTeam: {
+        team: {
+            team: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true
+            },
+            hits: {
+                type: Number,
+                required: false
+            },
+            runs: {
+                type: Number,
+                required: false
+            },
+            errors: {
+                type: Number,
+                required: false
+            },
+            inningScores: {
+                type: [Number],
+                required: false
+            }
+        }
+    }
+});
+
+export default mongoose.model("game", GameSchema);
